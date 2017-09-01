f7d02a3f15168cc42fcac933f4996b8b10eb882f
FPGA-Computer <fpga.computer.001@gmail.com>
2017-09-01 17:29:35 -0400

    compiled version

--- Firmware/Atmel Studio 7/VUSB-HID/Debug/main.d
+++ Firmware/Atmel Studio 7/VUSB-HID/Debug/main.d
@@ -18,8 +18,7 @@ main.d main.o: .././main.c .././hardware.h \
  .././usbdrv/usbportability.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
- .././main.h
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
 
 .././hardware.h:
 
@@ -68,5 +67,3 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
-
-.././main.h:
